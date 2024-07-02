64efa5fbdf2919b4ca5a91649bfd6c7ea1c6bce1
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-07-02 10:23:50 +0530

    Events Example along with Assessment Hack code

---
+++ DotnetTraining/Frameworks/AssignmentHack.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkExamples
+{
+    class StringEncoder
+    {
+        const string CODE = "the quick and brown fox jumps over the lazy dog";
+        static List<string> words = CODE.Split(' ').ToList();
+        public static string Encode(string input)
+        {
+            if (input == null) throw new NullReferenceException("Arg cannot be null");
+            if (string.IsNullOrEmpty(input)) throw new ArgumentException("Arg cannot be empty");
+
+            var content = string.Empty;
+
+            foreach (char c in input.ToLower())
+            {
+                if (c == ' ')
+                {
+                    content = content.Substring(0, content.Length - 1);
+                    content += getCode(c);
+                }
+                else
+                {
+                    content += getCode(c);
+                    content += ',';
+                }
+            }
+            return content.Substring(0, content.Length - 1);
+
+        }
+
+        public static string getCode(char ch)
+        {
+            if (ch == ' ') return "-";
+            string code = string.Empty;
+            foreach (var word in words)
+            {
+                if (word.Contains(ch))
+                {
+                    code += words.IndexOf(word);
+                    code += word.IndexOf(ch);
+                    return code;
+                }
+            }
+            return code;
+        }
+    }
+
+    class UniqueAndDuplicate
+    {
+        public static Dictionary<string, List<string>> GetDuplicatesAndUnique(string input)
+        {
+            Dictionary<string, List<string>> pairs = new Dictionary<string, List<string>>();
+            pairs["Unique"] = new List<string>();
+            pairs["Duplicates"] = new List<string>();
+            foreach(var word in input.ToLower().Split(" "))
+            {
+                if (pairs["Unique"].Contains(word))
+                {
+                    if(!pairs["Duplicates"].Contains(word))
+                        pairs["Duplicates"].Add(word);
+                    pairs["Unique"].Remove(word);
+                }
+                else
+                {
+                    pairs["Unique"].Add(word); 
+                }
+            }
+            return pairs;
+        }
+    }
+    internal class AssignmentHack
+    {
+        static void Main(string[] args)
+        {
+            //testForBangalore();
+            //testForMindtree();
+            //testForGlobalVillage();
+            testForUniqueValues();
+        }
+
+        private static void testForUniqueValues()
+        {
+            var actual = UniqueAndDuplicate.GetDuplicatesAndUnique("Do not call me let me call you");
+            var unique = new List<string> { "Do", "let", "not", "you" };
+            var duplicates = new List<string> { "call", "me" };
+
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("Unique", unique);
+            expected.Add("Duplicates", duplicates);
+            var uniques = actual["Unique"];
+            Console.Write("Unique: ");
+            foreach (var item in uniques)
+            {                
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            var dupes = actual["Duplicates"];
+            Console.Write("Duplicates: ");
+            foreach (var e in dupes)
+            {
+                Console.Write(e + " ");
+            }
+
+        }
+
+        private static void testForGlobalVillage()
+        {
+            var actual = StringEncoder.Encode("Global Village");
+            var expected = "92,80,32,30,20,80-61,12,80,80,20,92,02";
+            if (actual == expected)
+            {
+                Console.WriteLine("Tests have passed");
+            }
+            else
+            {
+                Console.WriteLine("Tests have failed");
+            }
+        }
+
+        private static void testForMindtree()
+        {
+            var actual = StringEncoder.Encode("mindtree");
+            var expected = "52,12,21,22,00,31,02,02";
+            if (actual == expected)
+            {
+                Console.WriteLine("Tests have passed");
+            }
+            else
+            {
+                Console.WriteLine("Tests have failed");
+            }
+        }
+
+        private static void testForBangalore()
+        {
+            var actual = StringEncoder.Encode("bangalore");
+            var expected = "30,20,21,92,20,80,32,31,02";
+            if (actual == expected)
+            {
+                Console.WriteLine("Tests have passed");
+            }
+            else
+            {
+                Console.WriteLine("Tests have failed");
+            }
+        }
+    }
+}
---
+++ DotnetTraining/Frameworks/Ex04EventsExample.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Channels;
+using System.Threading;
+namespace FrameworkExamples
+{
+    /*
+     * Events:
+     * Actions performed on the object is called as Events. 
+     * Click, Press, Move are some of the events that are handled at the UI level on UI components.
+     * Events in C# classes will be like callback functions that are invoked when a certain condition or an operation is done.
+     * All Events are objects(instances) of delegates in C#. They are declared using a keyword event. You shall invoke the instance at the approprite condition of the program execution. 
+     * We can use the .NET provided delegates called Action<T> and Func<T>. These delegates are template based and can accomodate any no(16) of parameters. 
+     * Func<T> is used for Functions that return value and Action is used for void Functions. 
+     */
+
+    class AlarmClock
+    {
+        public event Action OnAlarmTime;
+        private readonly DateTime alarmTime;
+        public AlarmClock(DateTime alarmTime)
+        {
+            this.alarmTime = alarmTime;
+        }
+
+        public void DisplayClock()
+        {
+            if (DateTime.Now.Minute == alarmTime.Minute)
+            {
+                //Raise the event...
+                if(OnAlarmTime != null)
+                    OnAlarmTime();
+                else
+                    Console.WriteLine("Event handler is not set");
+            }
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        }
+    }
+    class EventsExample
+    {
+        static void testFunc()
+        {
+            Console.WriteLine("Test Func");
+        }
+        static void Main(string[] args)
+        {
+            //ActionExample();
+            ClockExample();
+        }
+
+        private static void ClockExample()
+        {
+            AlarmClock clock = new AlarmClock(DateTime.Now.AddMinutes(1));
+            clock.OnAlarmTime += Clock_OnAlarmTime;
+            do
+            {
+                clock.DisplayClock();
+                Thread.Sleep(1000);//1 sec stopage....
+                Console.Clear();
+            } while (true);
+        }
+
+        private static void Clock_OnAlarmTime()
+        {
+            Console.WriteLine("Time for break!!!");
+        }
+
+        //private static void onAlarmRaised()
+        //{
+
+        //}
+
+        private static void ActionExample()
+        {
+            Action example = () => Console.WriteLine("Using Anonymous methods");
+
+            example();
+        }
+    }
+}
