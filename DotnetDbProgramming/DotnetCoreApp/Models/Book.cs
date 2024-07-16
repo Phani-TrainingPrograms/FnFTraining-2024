1aed66bcc1f2df06b5215783000801379994dca1
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-07-16 12:46:05 +0530

    Db Programming

---
+++ DotnetDbProgramming/DotnetCoreApp/Models/Book.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCoreApp.Models
+{
+    [Table("BookTable")]
+    internal class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } =string.Empty;
+        public int Price { get; set; }
+    }
+
+    //This class represents the interaction that U do with the database. This class is derived from a EF Library class called DbContext. 
+    class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; } //This property represents the data of the BookTable U create. 
+
+        private const string DB_SOURCE = ".\\SQLEXPRESS";
+        private const string DB_NAME = "FnfTraining";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var connectionString = $"Data Source={DB_SOURCE};Initial Catalog={DB_NAME};Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    //Open Package manager Console and run the following commands:
+    //add-migration mig1
+    //update-database
+
+
+
+
+}
---
+++ DotnetDbProgramming/DotnetCoreApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DotnetCoreApp
+{
+    using DotnetCoreApp.Models;
+    using System;
+
+    //To intearct with databases, we use EF Core. EF Core is available for all kinds of databases.
+    //We use Nuget packages to download the packages requried to use EF. 
+    //MS EF Core, MS EFTools, MS EF SqlServer.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            addingFeature();
+           
+        }
+
+        private static void addingFeature()
+        {
+            //create a book object to represent the data to insert
+            var rec = new Book
+            {
+                Author = "James Clear",
+                Title = "Atomic Habits",
+                Price = 400
+            };
+
+            //Create the DbContext 
+            var dbcontext = new BookContext();
+            dbcontext.Books.Add(rec);
+            dbcontext.SaveChanges();
+        }
+    }
+}
