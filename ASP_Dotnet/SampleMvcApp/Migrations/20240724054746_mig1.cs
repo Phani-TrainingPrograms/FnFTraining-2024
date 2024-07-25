0acbaa220b91e7c296fb8c296f2597449c593ced
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-07-25 10:19:53 +0530

    ASP.NET MVC CORE 8.0

---
+++ ASP_Dotnet/SampleMvcApp/Migrations/20240724054746_mig1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleMvcApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeTable",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpSalary = table.Column<int>(type: "int", nullable: false),
+                    EmpImage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTable", x => x.EmpId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeTable");
+        }
+    }
+}
