ce4b06b236f51ee927255c114dd4c3a2673eee12
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-07-29 17:09:03 +0530

    web api

---
+++ ASP_Dotnet/SampleWebApi/Controllers/StockMarketController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebApi.Data;
+
+
+//Scaffold-DbContext "Server=.\SQLEXPRESS; Database=FnfTraining; Trusted_Connection=True; TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockMarketController : ControllerBase
+    {
+        private readonly FnfTrainingContext _context;
+
+        public StockMarketController(FnfTrainingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Stocks")]
+        public List<StockTable> AllStocks()
+        {
+            var result = _context.StockTables.ToList();
+            return result;
+        }
+
+        [HttpPost]
+        [Route("Stocks")]
+        public ObjectResult AddStock(StockTable stock)
+        {
+            _context.StockTables.Add(stock);
+            _context.SaveChanges();
+            return Ok("Added Successfully");
+        }
+    }
+}
