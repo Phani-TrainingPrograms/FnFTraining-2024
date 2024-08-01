21882e57398bad0127298d9767324a94385209d2
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-08-01 16:56:04 +0530

    Delete ASP_Dotnet directory

--- ASP_Dotnet/SampleMvcApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SampleMvcApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
