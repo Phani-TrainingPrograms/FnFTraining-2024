84c6bf42167f0d3dbf7249ff4944ac4e45c96ea1
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-08-08 15:28:19 +0530

    updated with @Input Directive

--- Angular/demo-app/src/app/employee/emp-list/emp-list.component.ts
+++ Angular/demo-app/src/app/employee/emp-list/emp-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../Models/employee';
 import { CommonModule } from '@angular/common';
+import { EmpViewComponent } from '../emp-view/emp-view.component';
 
 @Component({
   selector: 'app-emp-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EmpViewComponent],//UR component is using EmpView
   templateUrl: './emp-list.component.html',
   styleUrl: './emp-list.component.css'
 })
@@ -13,22 +14,22 @@ export class EmpListComponent implements OnInit {
   ngOnInit(): void {
     //Use ngOnInit to initialize the data of UR component.
     this.empList.push({
-      empId : 123, empName : "Phaniraj", empAddress : "Bangalore", empSalary : 56000, empPic : 'assets/images/pic1.png'
+      empId : 123, empName : "Phaniraj", empAddress : "Bangalore", empSalary : 56000, empPic : 'pic1.png'
     });
     this.empList.push({
-      empId : 124, empName : "Banu Prakash", empAddress : "Mysore", empSalary : 16000, empPic : 'assets/images/pic6.png'
+      empId : 124, empName : "Banu Prakash", empAddress : "Mysore", empSalary : 16000, empPic : 'pic6.png'
     });
     this.empList.push({
-      empId : 125, empName : "Vinod Kumar", empAddress : "Shimogga", empSalary : 56000, empPic : 'assets/images/pic5.png'
+      empId : 125, empName : "Vinod Kumar", empAddress : "Shimogga", empSalary : 56000, empPic : 'pic5.png'
     });
     this.empList.push({
-      empId : 126, empName : "Joydip Mondal", empAddress : "Kolkatta", empSalary : 56000, empPic : 'assets/images/pic4.png'
+      empId : 126, empName : "Joydip Mondal", empAddress : "Kolkatta", empSalary : 56000, empPic : 'pic4.png'
     });
     this.empList.push({
-      empId : 127, empName : "Ramnath Nishad", empAddress : "Patna", empSalary : 56000, empPic : 'assets/images/pic3.png'
+      empId : 127, empName : "Ramnath Nishad", empAddress : "Patna", empSalary : 56000, empPic : 'pic3.png'
     });
     this.empList.push({
-      empId : 128, empName : "Phaniraj", empAddress : "Bangalore", empSalary : 56000, empPic : 'assets/images/pic2.png'
+      empId : 128, empName : "Phaniraj", empAddress : "Bangalore", empSalary : 56000, empPic : 'pic2.png'
     });
 
   }
--- Angular/demo-app/src/app/employee/emp-view/emp-view.component.ts
+++ Angular/demo-app/src/app/employee/emp-view/emp-view.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Employee } from '../Models/employee';
 
 @Component({
   selector: 'app-emp-view',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './emp-view.component.css'
 })
 export class EmpViewComponent {
-
+  //When we want to transfer any data from the parent component to the child component, we can use Input Directive with the variable name. When this component is refered in the parent UI, it shall use the selector and pass a property bound value to this component. 
+  @Input() empObj = {} as Employee; 
 }
